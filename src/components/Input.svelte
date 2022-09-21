b1181590ec72abbd60f4354926f6e106006ad85f
Gavin Daly <gavdaly@protonmail.com>
2022-09-21 01:03:53 -0400

    initial commit

---
+++ src/components/Input.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let label: string;
+  export let value: string;
+  export let required: boolean;
+</script>
+
+<div class="input">
+  <label for={label}>{label}</label>
+  <input
+    type="text"
+    name={label}
+    id={label}
+    bind:value
+    autocomplete="email"
+    inputmode="email"
+    {required}
+  />
+</div>
+
+<style>
+  .input {
+    display: flex;
+    padding-bottom: var(--size-3);
+    flex-direction: column;
+    width: 100%;
+  }
+  input {
+    border: none;
+    padding: 0.4rem;
+    width: auto;
+  }
+</style>
---
+++ src/components/ListForm.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Input from "../components/Input.svelte";
+
+  $: form = { email: "", name: "" };
+
+  function submit(e) {
+    console.log(form);
+  }
+</script>
+
+<form on:submit={submit}>
+  <Input bind:value={form.name} label="name" required />
+  <Input bind:value={form.email} label="email" required />
+  <button type="submit">join</button>
+</form>
+
+<style scoped>
+  form {
+    padding: var(--size-3);
+    background: #00000022;
+    box-shadow: var(--shadow-2);
+    max-width: 40ch;
+    border-radius: var(--radius-3);
+    margin-top: var(--size-3);
+    margin-bottom: var(--size-3);
+  }
+</style>
